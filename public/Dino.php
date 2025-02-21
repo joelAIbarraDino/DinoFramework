62506d3c8eca77c5f63d66ddcd66e7c342fd50dd
Joel Alejandro Ibarra Villar <joelvillar35@gmail.com>
2025-02-20 22:15:09 -0600

    Correcion en funcion de envio de correo en clase Dino

--- public/Dino.php
+++ public/Dino.php
@@ -39,23 +39,26 @@ class Dino{
 
     private Logger $logger;
     private ?PHPMailer $mailer;
+    private array $emailConfig;
 
-    public function __construct(string $nameApp, string $rootDir, array $DBconfig = [], string $DBdriver = Database::PDO_DRIVER, int $mode = self::DEVELOPMENT_MODE, array $emailConfig = null){
+    public function __construct(string $nameApp, string $rootDir, array $DBconfig = [], string $DBdriver = Database::PDO_DRIVER, int $mode = self::DEVELOPMENT_MODE, array $emailConfig){
         
         self::$ROOT_DIR = $rootDir;
         self::$APP_NAME = $nameApp;
-        $this->mailer = null;
         
+        $this->response = new Response(self::$ROOT_DIR);
+        $this->emailConfig = $emailConfig??null;
+        $this->request = new Request;
+        $this->router = new Router;
+        $this->mailer = null;
+
         $this->setupLogger();
-        $this->setupErrorHandling($mode, $emailConfig);
+        $this->setupErrorHandling($mode);
 
         if(empty($DBconfig))
             throw new InvalidArgumentException("config array must be configured");
 
         $this->database = new Database($DBconfig, $DBdriver);
-        $this->response = new Response(self::$ROOT_DIR);
-        $this->request = new Request;
-        $this->router = new Router;
     }
 
     private function setupLogger(): void{
@@ -68,14 +71,14 @@ class Dino{
         $this->logger->pushHandler(new StreamHandler($logFile, Logger::ERROR));
     }
 
-    private function setupErrorHandling(int $mode, array $emailConfig): void{
+    private function setupErrorHandling(int $mode): void{
         $whoops = new Run;
 
         switch($mode){
             case self::PRODUCTION_MODE:
-                $whoops->pushHandler(new CallbackHandler(function ($error, $emailConfig) {
+                $whoops->pushHandler(new CallbackHandler(function ($error) {
                     $this->logError($error);
-                    $this->sendErrorEmail($error, $emailConfig);
+                    $this->sendErrorEmail($error);
                     echo "Ha ocurrido un error. Por favor, inténtalo de nuevo más tarde.";
                 }));
             break;
@@ -94,18 +97,18 @@ class Dino{
         ]);
     }
     
-    private function sendErrorEmail($error, $emailConfig): void
+    private function sendErrorEmail($error): void
     {
-        if(is_null($emailConfig))
+        if(is_null($this->emailConfig))
             return;
         
         if($this->mailer === null)
-            $this->setupMailer($emailConfig);
+            $this->setupMailer($this->emailConfig);
 
 
         try{
-            $this->mailer->setFrom($emailConfig['from'], self::$APP_NAME);
-            $this->mailer->addAddress($emailConfig['to'], $emailConfig['name']);
+            $this->mailer->setFrom($this->emailConfig['from'], self::$APP_NAME);
+            $this->mailer->addAddress($this->emailConfig['to'], $this->emailConfig['name']);
             $this->mailer->Subject = 'Error en la aplicación '.self::$APP_NAME;
             $this->mailer->Body = sprintf(
                 "Ha ocurrido un error en la aplicación:\n\nMensaje: %s\n\nTraza:\n%s",
@@ -123,11 +126,11 @@ class Dino{
         $this->mailer = new PHPMailer(true);
 
         $this->mailer->isSMTP();
+        $this->mailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $this->mailer->Password = $emailConfig['password'];
+        $this->mailer->Username = $emailConfig['user'];
+        $this->mailer->Port = $emailConfig['port'];
         $this->mailer->Host = $emailConfig['host'];
         $this->mailer->SMTPAuth = true;
-        $this->mailer->Username = $emailConfig['user'];
-        $this->mailer->Password = $emailConfig['password'];
-        $this->mailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $this->mailer->Port = $emailConfig['port'];
     }
 }
