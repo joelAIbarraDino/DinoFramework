5cb8af236e6a52da5ece6f0e68e7caada0e1e6dc
Joel Alejandro Ibarra Villar <joelvillar35@gmail.com>
2025-02-21 23:21:52 -0600

    reorganizacion de parametros del constructor

--- public/Dino.php
+++ public/Dino.php
@@ -41,7 +41,7 @@ class Dino{
     private ?PHPMailer $mailer;
     private ?array $emailConfig;
 
-    public function __construct(string $nameApp, string $rootDir, ?array $DBconfig = null, int $mode = self::DEVELOPMENT_MODE, ?array $emailConfig = null){
+    public function __construct(string $nameApp, string $rootDir, int $mode = self::DEVELOPMENT_MODE, ?array $DBconfig = null, ?array $emailConfig = null){
         
         self::$ROOT_DIR = $rootDir;
         self::$APP_NAME = $nameApp;
