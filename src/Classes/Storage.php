5d79a66c1652cc471fc662939394f6303274aea0
joelAIbarraDino <joel.ibarra@dinozign.com>
2025-04-06 02:25:06 -0600

    se agrega clase Storage para manejo de archivos

---
+++ src/Classes/Storage.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DinoEngine\Classes;
+
+class Storage{
+
+    public static function exists(string $path):bool{
+        if(empty($path))
+            return false;
+        
+        return file_exists($path);
+    }
+
+    public static function delete(string $path):bool{
+        if(empty($path))
+            return false;
+
+        return unlink($path);
+    }
+
+    public static function save(string $file, string $path):bool{
+        if(empty($path) || empty($file))
+            return false;
+
+        if(!is_dir($path))
+            mkdir($path);
+
+        if(!move_uploaded_file($file, $path))
+            return false;
+
+        return true;
+    }
+
+}
+
